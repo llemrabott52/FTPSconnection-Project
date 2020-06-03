e7c8d1d9a173c92f7fb9dbf3bf16aebabdfda382
llemrabott52 <42483583+llemrabott52@users.noreply.github.com>
2020-06-03 15:44:33 +0100

    Add files via upload

---
+++ ProjectSpringNewVersion/ComposantRepository.java
@@ -0,0 +1,9 @@
+package com.monetique.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.monetique.entities.Composant;
+
+public interface ComposantRepository extends CrudRepository<Composant, String>{
+
+}
---
+++ ProjectSpringNewVersion/ParameterRepository.java
@@ -0,0 +1,9 @@
+package com.monetique.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.monetique.entities.Parameter;
+
+public interface ParameterRepository extends CrudRepository<Parameter, Long>{
+
+}
